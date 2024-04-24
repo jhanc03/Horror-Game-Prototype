00cd46d2fe4e8edafe47f20e27751d925eb307be
jhanc03 <jhancock1303@gmail.com>
2024-04-24 15:44:17 +0100

    Started work on SFX

--- Horror_Game_Prototype/Assets/Scripts/GameController.cs
+++ Horror_Game_Prototype/Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GameController : MonoBehaviour
 {
@@ -9,9 +10,12 @@ public class GameController : MonoBehaviour
 
     //Doors
     public Transform lDoor, rDoor;
-    bool lDoorClosed, rDoorClosed
-        ,lDoorClosing, lDoorOpening, rDoorClosing, rDoorOpening;
+    bool lDoorClosed, rDoorClosed,
+         lDoorClosing, lDoorOpening, rDoorClosing, rDoorOpening;
     const float doorRotSpeed = 63.41f;
+
+    //Monster
+    GameObject monster;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +28,8 @@ public class GameController : MonoBehaviour
         lDoorOpening = false;
         rDoorClosing = false;
         rDoorOpening = false;
+
+        monster = GameObject.FindGameObjectWithTag("Monster");
     }
 
     // Update is called once per frame
@@ -54,7 +60,6 @@ public class GameController : MonoBehaviour
         if (rDoorClosing)
         {
             rDoor.Rotate(Vector3.up, -(doorRotSpeed * Time.deltaTime));
-            Debug.Log(rDoor.localEulerAngles.y);
             if (rDoor.localEulerAngles.y > 0.0f && rDoor.localEulerAngles.y < 1.0f)
             {
                 rDoor.localEulerAngles = Vector3.zero;
@@ -73,19 +78,28 @@ public class GameController : MonoBehaviour
             }
         }
 
-    //https://docs.unity3d.com/Manual/ProgressiveLightmapper-UVOverlap.html
+		//Monster
+		//Positions:
+		//Cam 6 = Vector3(-28.653,0,18.11), Vector3(-30.531,0,21.747)
+		//Cam 5 = Vector3(-23.52,0,9.87), Vector3(-26.37,0,4.96), Vector3(-23.59,0,-6.74)
+		//Cam 4 = Vector3(-30.15,0,16.2), Vector3(-23.552,0,12.069), Vector3(-20.4529991,0,17.2259998)
+		//Cam 3 = Vector3(-13.77,0,-8.46), Vector3(-16.044,0,8.186)
+		//Cam 2 = Vector3(-11.54,0,6.51), Vector3(-4.413,0,1.126)
+		//Cam 1 = Vector3(-11.78,0,-3.18), Vector3(-5.505,0,-8.497)
+		//LDoor = Vector3(0.156,0,-2.914)
+		//RDoor = Vector3(0.156,0,2.635)
+	}
 
-    }
 
 
-
-    //Controls
-    public void ToggleCameras()
+	//Controls
+	public void ToggleCameras()
     {
         cameraManager.ToggleCameras();
     }
+	//https://docs.unity3d.com/Manual/ProgressiveLightmapper-UVOverlap.html
 
-    public void ToggleLeftDoor()
+	public void ToggleLeftDoor()
     {
         if (!(lDoorClosing || lDoorOpening))
         {
---
+++ Horror_Game_Prototype/Assets/Scripts/SFXController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXController : MonoBehaviour
+{
+    public AudioClip amb1, amb2, amb3, amb4, amb5;
+
+	public AudioClip sfx1, sfx2, sfx3, sfx4, sfx5, sfx6, sfx7;
+
+	public AudioClip jmp1, jmp2, jmp3, jmp4, jmp5;
+
+    public AudioClip msc1, msc2, msc3, msc4, msc5;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
